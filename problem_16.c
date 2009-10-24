0ededf03ced7af5e02956ec7f1e3d9b1b5cada2f
Thomas <serenity@exscape.org>
2009-10-24 20:00:56 +0200

    Fix problem 16, soon to be reorganized

--- problem_16.c
+++ problem_16.c
@@ -4,23 +4,24 @@
 #include <stdint.h>
 #include <gmp.h>
 
-/* Written: 2009-10-24(, ?) */
+/* Written: 2009-10-24 */
 
 int main() {
 	mpz_t num;
-	mpz_init(num);
+	mpz_init2(num, 1000); /* initialize with 1000 bits */
 
 	// num = int_pow(2,1000)
 	mpz_ui_pow_ui(num, 2, 1000);
 
 	uint64_t sum = 0;
 
+//  while (num != 0) 
 	while (mpz_cmp_ui(num, 0) != 0) {
 //		sum += num % 10;
 		sum += mpz_fdiv_ui(num, 10);
 
 //		num /= 10;
-//		???
+		mpz_tdiv_q_ui(num, num, 10);
 	}
 
 	printf("Answer: %llu\n", sum);
