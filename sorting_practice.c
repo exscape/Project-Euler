72a4fafb28ca726cb59383a41f842e0ace54ecbd
Thomas <serenity@exscape.org>
2009-10-27 11:07:39 +0100

    Add sorting_practice.c - I'll try to write a quicker algorithm WITHOUT any help.

---
+++ sorting_practice.c
@@ -0,0 +1,45 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <stdint.h>
+
+void int_print_array(int arr[], int len) {
+	for (int i=0; i < len; i++) {
+		printf("%d ", arr[i]);
+	}
+	printf("\n");
+}
+
+inline void int_swap (int *n1, int *n2) {
+	int tmp = *n2;
+	*n2 = *n1;
+	*n1 = tmp;
+}
+
+void int_sort(int arr[], int num_elements) {
+	// Idea #1:
+	// Keep looping through the list from the start until it's sorted; if an out-of-place element is found,
+	// swap it with the current one.
+	// 3 1 4 2 -> 1 3 4 2 -> 1 3 2 4 -> 1 2 3 4
+	// Bubble sort, right?
+	//
+	// Idea #2:
+	// Bubble sort sucks.
+	for (int16_t i = 0; i < num_elements-1; i++) {
+		if (arr[i+1] < arr[i]) {
+			int_swap(&arr[i+1], &arr[i]);
+			i = -1; // restart loop
+			continue;
+		}
+	}
+}
+
+int main() {
+	int arr[] = {4, 10, 61, 2, 9, 0, -6, 5, -1, -2};
+	int num_elements = sizeof(arr)/sizeof(int);
+
+	int_print_array(arr, num_elements);
+	int_sort(arr, num_elements);
+	int_print_array(arr, num_elements);
+
+	return 0;
+}
