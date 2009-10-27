3f3092c9713d30ead24fe813fff73f73b6318224
Thomas <serenity@exscape.org>
2009-10-27 15:34:18 +0100

    No NUL termination is necessary, since we loop to a known offset only

--- problem_8.c
+++ problem_8.c
@@ -40,19 +40,18 @@ int main() {
 	}
 	fclose(f); f = NULL;
 
-	char *num = malloc(1000 + 1); /* 1000 digits + 1 NUL */
+	char *num = malloc(1000); /* 1000 digits, no NUL */
 	char *num_p = num;
 	for (char *buf_p = buf; *buf_p != '\0'; buf_p++) {
 		if (*buf_p >= '0' && *buf_p <= '9')
 			*num_p++ = (*buf_p) - 0x30; /* Convert from '1' to (int)1 etc. */
 	}
-	num[1000] = '\0';
 
 	free(buf); buf = NULL;
 
 	/* Finally, time for business */
 	uint16_t max = 0, curr; /* Max possible value = 9^5 which is less than 2^16, so 16 bits is enough */
-	for (uint16_t i = 0; i <= 1000-4; i++) { /* OBOE? */
+	for (uint16_t i = 0; i < 1000-4; i++) {
 		curr = num[i] * num[i+1] * num[i+2] * num[i+3] * num[i+4];
 		if (curr > max)
 			max = curr;
