6d6d4d1bd4af4309d9fc067c3c8821330495f3b3
Thomas <serenity@exscape.org>
2009-10-28 18:58:32 +0100

    Massive improvements to problem 29, including a whole new datatype for sets w/ dynamic memory resizing!

--- problem_29.c
+++ problem_29.c
@@ -3,13 +3,35 @@
 #include <math.h>
 #include <stdint.h>
 #include <gmp.h>
+#include <string.h>
 
-/* Written: 2009-10-28, XXX */
+/* Written: 2009-10-28 */
 
-uint8_t in_set(mpz_t haystack[] , const uint32_t num_elements, const mpz_t needle) {
-	for (int32_t i = 0; i < num_elements; i++) {
-		if (!mpz_cmp(haystack[i], needle)) {
-			comparisons++;
+/* Holds a set (an array unique elements) of mpz_t variables */
+typedef struct {
+	mpz_t *arr; // The data storage itself
+	size_t size; // The currently allocated size (in elements)
+	size_t num_elements; // The number of elements used
+} gmp_int_set;
+
+/* Initialize a set (i.e. allocate memory and set variables) */
+void gmp_int_set_alloc(gmp_int_set *set, size_t size) {
+	set->arr = malloc(size * sizeof(mpz_t));
+	set->size = size;
+	set->num_elements = 0;
+}
+
+void gmp_int_set_free(gmp_int_set *set) {
+	for (size_t i = 0; i < set->num_elements; i++)
+		mpz_clear(set->arr[i]);
+	free(set->arr);
+	free(set);
+}
+
+// XXX: Linear search in unsorted set, better than sorting and using a binary search, or not?
+uint8_t in_set(const gmp_int_set *haystack, const mpz_t needle) {
+	for (int32_t i = 0; i < haystack->num_elements; i++) {
+		if (!mpz_cmp(haystack->arr[i], needle)) {
 			return 1;
 		}
 	}
@@ -17,46 +39,51 @@ uint8_t in_set(mpz_t haystack[] , const uint32_t num_elements, const mpz_t needl
 }
 
 /*
-static int mpz_cmpfunc(const void *in1, const void *in2) {
-	mpz_t *m1 = (mpz_t *)in1;
-	mpz_t *m2 = (mpz_t *)in2;
-	return mpz_cmp(*m1, *m2);
-}
-*/
-
-// XXX: How do we implement add_to_set()? static variables - which would (??) make it MP-unsafe? Caller handles
-// all? Since the memory management must be dynamic, someone has to keep track of the current allocation size.
-
-// Temporary ONLY!
-static mpz_t set[10000];
-static size_t i = 0;
-static uint32_t comparisons = 0;
-
-void add_to_set(mpz_t n) {
-	if (!in_set(set, i, n))
-		mpz_set(set[i++], n);
-}
-
-#define MAX 100
-
-int main() {
-	mpz_t result;
-	mpz_init(result);
-	for (uint64_t a = 2; a <= MAX; a++) {
-		for (uint64_t b = 2; b <= MAX; b++) {
-			mpz_ui_pow_ui(result, a, b);
-			add_to_set(result);
+ * set: the set to work with
+ * n: the number to add to the set; duplicates will not be added, or it wouldn't be a set!
+ */
+void add_to_set(gmp_int_set *set, mpz_t n) {
+	if (!in_set(set, n)) {
+		/* make sure that we don't step over our boundaries */
+		if (set->num_elements + 1 > set->size) {
+			set->arr = realloc(set->arr, 2 * set->size * sizeof(mpz_t)); // XXX: 2* ?
+			set->size *= 2;
+			if (set->arr == NULL) {
+				fprintf(stderr, "Failed to add to set: realloc() failed\n");
+				exit(1);
+			}
 		}
+		mpz_set(set->arr[set->num_elements++], n);
 	}
-	mpz_clear(result);
+}
 
 /*
-	for (int j=0; j<i; j++) {
-		gmp_printf("%Zd\n", set[j]);
-	}
+inline size_t set_length(gmp_int_set *set) {
+	return set->num_elements;
+} 
 */
+// for GDB...
+#define set_length(set) ((set->num_elements))
 
-	printf("Answer: %zu (%u comparisons)\n", i, comparisons);
+int main() {
+	gmp_int_set *set = malloc(sizeof(*set));
+	gmp_int_set_alloc(set, 200); /* initial storage for 200 numbers, which is less than enough, to test realloc */
+
+	mpz_t result;
+	mpz_init(result);
+
+#define MAX 100 /* 2 <= a <= MAX, same for b */
+	for (uint64_t a = 2; a <= MAX; a++) {
+		for (uint64_t b = 2; b <= MAX; b++) {
+			mpz_ui_pow_ui(result, a, b); /* result = a^b */
+			add_to_set(set, result);
+		}
+	}
+
+	printf("Answer: %zu\n", set_length(set));
+
+	mpz_clear(result);
+	gmp_int_set_free(set);
 
 	return 0;
 }
