9779b5b4ecd6b0df022aa4bdda09f671a123e6b6
Thomas <serenity@exscape.org>
2009-10-28 16:56:31 +0100

    Commit working, but still way sub-par solution for problem 29

--- problem_29.c
+++ problem_29.c
@@ -3,8 +3,20 @@
 #include <math.h>
 #include <stdint.h>
 #include <gmp.h>
+#include <search.h>
 
 /* Written: 2009-10-28 */
+
+uint8_t in_set(const mpz_t *haystack, const uint32_t num_elements, const mpz_t needle) {
+	uint32_t loops = 0;
+	for (int32_t i = 0; i < num_elements; i++) {
+		loops++;
+		if (!mpz_cmp(haystack[i], needle)) {
+			return 1;
+		}
+	}
+	return 0;
+}
 
 static int mpz_cmpfunc(const void *in1, const void *in2) {
 	mpz_t *m1 = (mpz_t *)in1;
@@ -17,43 +29,33 @@ static int mpz_cmpfunc(const void *in1, const void *in2) {
 
 // Temporary ONLY!
 static mpz_t set[10000];
-static uint32_t i = 0;
-
-uint8_t in_set(mpz_t n) {
-	qsort(set, i, sizeof(mpz_t), mpz_cmpfunc); // works; XXX: move to add_to_set
-	gmp_printf("in_set called with n = %Zd\n", n);
-	mpz_t *p = bsearch(&n, &set, i, sizeof(mpz_t), mpz_cmpfunc);
-	if (p == NULL) {
-		gmp_printf(" ... bsearch returned NULL for %Zd\n", n);
-		return 0;
-	}
-	else {
-		gmp_printf(" ... bsearch returned %Zd\n", *p);
-		return 1;
-	}
-}
+static size_t i = 0;
 
 void add_to_set(mpz_t n) {
-	if (!in_set(n))
+	if (!in_set(set, i, n))
 		mpz_set(set[i++], n);
 }
+
+#define MAX 100
 
 int main() {
 	mpz_t result;
 	mpz_init(result);
-	for (uint64_t a = 2; a <= 5; a++) {
-		for (uint64_t b = 2; b <= 5; b++) {
+	for (uint64_t a = 2; a <= MAX; a++) {
+		for (uint64_t b = 2; b <= MAX; b++) {
 			mpz_ui_pow_ui(result, a, b);
 			add_to_set(result);
 		}
 	}
 	mpz_clear(result);
 
+/*
 	for (int j=0; j<i; j++) {
 		gmp_printf("%Zd\n", set[j]);
 	}
+*/
 
-	printf("Answer: %u\n", i);
+	printf("Answer: %zu\n", i);
 
 	return 0;
 }
