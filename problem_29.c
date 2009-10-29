90c8423d7e2af8a4a8969560b580b52d192e8576
Thomas <serenity@exscape.org>
2009-10-29 09:30:52 +0100

    Update comments, fix segfault when size is too small, remove check from alloc() since we not handle it automatically; still needs cleaning up

--- problem_29.c
+++ problem_29.c
@@ -5,10 +5,10 @@
 #include <gmp.h>
 #include <string.h>
 
-/* Written: 2009-10-28 */
+/* Written: 2009-10-28 (working solution), 2009-10-29 (extra work on gmp_int_set) */
 
 //
-// XXX: TODO: Split the set functions into a "library"; add removal of elements, and possibly also memory
+// XXX: TODO: Split the set functions into a "library"; add removal of elements(?), and possibly also memory
 // deallocation when removing enough elements.
 //
 
@@ -25,20 +25,27 @@ typedef struct {
  * size: the number of elements to initally allocate memory for (will be resized as needed)
  */
 void gmp_int_set_alloc(gmp_int_set *set, size_t size) {
-	if (size <= 0) {
-		fprintf(stderr, "gmp_int_set_alloc called with size <= 0\n");
-		exit(1);
-	}
 	set->arr = malloc(size * sizeof(mpz_t));
 	set->size = size;
 	set->num_elements = 0;
 }
 
+/*
+ * Free the allocated memory for a set
+ * set: the set to free
+ * return value: none
+ */
 void gmp_int_set_free(gmp_int_set *set) {
 	for (size_t i = 0; i < set->num_elements; i++)
 		mpz_clear(set->arr[i]);
 	free(set->arr);
 	free(set);
+}
+
+static int mpz_cmpfunc(const void *in1, const void *in2) {
+        mpz_t *m1 = (mpz_t *)in1;
+        mpz_t *m2 = (mpz_t *)in2;
+        return mpz_cmp(*m1, *m2);
 }
 
 /*
@@ -49,6 +56,9 @@ void gmp_int_set_free(gmp_int_set *set) {
  * XXX: The performance is HORRIBLE - the runtime is reduced from 183ms to 1ms with a return 0; at the start here!
  */
 uint8_t in_set(const gmp_int_set *haystack, const mpz_t needle) {
+//	qsort(haystack->arr, haystack->num_elements, sizeof(mpz_t), mpz_cmpfunc);
+//	XXX: qsort + bsearch won't work, it's much too slow to sort the set this way. Linear search is 40 times faster.
+
 	for (int32_t i = 0; i < haystack->num_elements; i++) {
 		if (!mpz_cmp(haystack->arr[i], needle)) {
 			return 1;
@@ -62,27 +72,28 @@ uint8_t in_set(const gmp_int_set *haystack, const mpz_t needle) {
  * n: the number to add to the set; duplicates will not be added, or it wouldn't be a set!
  */
 void add_to_set(gmp_int_set *set, mpz_t n) {
+//		printf("num_elements = %zu, size = %zu\n", set->num_elements, set->size);
 	if (!in_set(set, n)) {
 		/* make sure that we don't step over our boundaries */
-		if (set->num_elements + 1 > set->size) {
-			set->arr = realloc(set->arr, 2 * set->size * sizeof(mpz_t)); // XXX: 2* ?
-			set->size *= 2;
+		if (set->num_elements + 2 > set->size) { // XXX: Why is +2 needed, and not just +1? (qsort segfaults with 1)
+			size_t new_size = set->size * 2;
+			if (new_size < 10)
+				new_size = 10;
+			set->arr = realloc(set->arr, new_size * sizeof(mpz_t));
+			set->size = new_size;
 			if (set->arr == NULL) {
 				fprintf(stderr, "Failed to add to set: realloc() failed\n");
 				exit(1);
 			}
 		}
-		mpz_set(set->arr[set->num_elements++], n);
+		mpz_init(set->arr[set->num_elements++]);
+		mpz_set(set->arr[set->num_elements], n);
 	}
 }
 
-/*
-inline size_t set_length(gmp_int_set *set) {
+/* inline */ size_t set_length(gmp_int_set *set) {
 	return set->num_elements;
 } 
-*/
-// for GDB...
-#define set_length(set) ((set->num_elements))
 
 int main() {
 	gmp_int_set *set = malloc(sizeof(*set));
