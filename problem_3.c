5e59b3f213d025b0983d7afc306f7543d8e6dee8
Thomas <serenity@exscape.org>
2009-10-30 20:52:46 +0100

    Speed up problem 3 with a factor of... uh, about 310 THOUSAND! *FACEPALM* My work with this code isn't done, though.

--- problem_3.c
+++ problem_3.c
@@ -7,10 +7,6 @@
 
 /* Written: 2009-10-17? */
 
-//
-// COMPLETELY UNACCEPTABLE: 31 minutes on a 3 GHz Core 2
-//
-
 #define NUM 600851475143
 //#define NUM 13195 /* Correct answer: 29 */
 
@@ -32,9 +28,8 @@ uint64_t *get_prime_factors(uint64_t n) {
 		*ptr++ = 2;
    uint64_t used_factors = 1; // 2, so far
 
-/*    uint64_t sqrt_n = (uint64_t)sqrt(n); */
-
-    for (uint64_t i=3; i <= (n/2)+3; i+=2) {  
+//    for (uint64_t i=3; i <= (n/2)+3; i+=2) {  // FACEPALM. Runtime reduced from 31m (MINUTES) to 6 ms (MILLISECONDS).
+    for (uint64_t i=3; i <= (uint64_t)sqrt(n); i+=2) {  
         if (n % i== 0 && isprime(i)) { 
             used_factors++;
 			*ptr++ = i;
@@ -42,9 +37,6 @@ uint64_t *get_prime_factors(uint64_t n) {
 				// factors++;
 			// }
         }
-/*		if (i % 10000001 == 0) 
-			printf("%.3f%%\n", ((float)i/(n/2))*100);
-*/
     }
 
 	return arr;
@@ -52,9 +44,12 @@ uint64_t *get_prime_factors(uint64_t n) {
 
 int main() {
 	uint64_t *arr = get_prime_factors(NUM);;
-	for (uint64_t i = 0; arr[i] != 0; i++) {
-		printf("Prime factor: %lu\n", arr[i]);
-	}
+	uint64_t i;
+	for (i = 0; arr[i] != 0; i++) 
+	
+	// The last element of the array is the largest and thus the answer;
+	// -1 is needed due to the above loop setting i to the NULL terminating element.
+	printf("Answer: %lu\n", arr[i-1]);
 
 	printf("Move get_divisors to util.c\n");
 	return 0;
