db8b14d7875739dac5f04e6ca75ee4dfd9f74019
Thomas <serenity@exscape.org>
2009-10-31 18:06:39 +0100

    More major rework on problem 3, including a new list_* set of functions for dynamic arrays; still a WIP

--- problem_3.c
+++ problem_3.c
@@ -5,104 +5,125 @@
 #include <string.h>
 #include "util.c"
 
-/* Written: 2009-10-17? */
+/* Written: 2009-10-17, + a lot of more days spent working on various memory allocation/dynamic array stuff for util.c */
 
 #define NUM 600851475143
 //#define NUM 26
 //#define NUM 13195 /* Correct answer: 29 */
 
-
 typedef struct {
-	size_t length;
-	uint64_t *arr;
+	size_t used; // The number of elements used
+	size_t size; // The size of the array in elements
+	uint64_t *arr; // The storage array itself
+	uint64_t *p; // A pointer to the current item in the array (XXX: (arr+used) instead?)
 } uint64_list;
 
-//
-// XXX: INCOMPLETE FUNCTION!
-// XXX: Perhaps even a whole array_*/list_* set of functions, with automatic memory management?
-//
-uint64_t *get_prime_factors(const uint64_t orig_n) {
-//	uint64list *list = calloc(1, sizeof(*list));
-	if (orig_n < 2) {
-		/* handle this edge case early; malloc() due to the fact that the caller
-		 * should be able to free() any return value */
-		uint64_t *i = malloc(sizeof(uint64_t));
-		if (i == NULL)
-			return NULL;
+/*
+ * Create a dynamically allocated list of uint64_ts
+ * return value: A preallocated list, or NULL if calloc fails.
+ */
+uint64_list *list_create(void) {
+	uint64_list *list = calloc(1, sizeof(*list));
+	if (list == NULL)
+		return NULL;
+	list->used = 0;
+	list->size = 200; // XXX: FIXME when memory management is working
+	list->arr = calloc(list->size, sizeof(uint64_t));
+	if (list->arr == NULL) {
+		return NULL;
+	}
+	list->p = list->arr;
 
-		*i = 0;
-		return i;
+	return list;
+}
+
+/*
+ * Free a list.
+ * list: The list to free; the pointer will also be set to NULL
+ * return value: none
+ */
+void list_free(uint64_list **list) {
+	free((*list)->arr);
+	free(*list);
+	*list = NULL;
+}
+
+/*
+ * Add a number to a list, allocating more memory if necessary
+ * list: the list to work with
+ * n: the number to add to the list
+ * return value: none
+ */
+void list_add(uint64_list **list, uint64_t n) {
+	// XXX: memory management!!
+	*(*list)->p++ = n;
+	(*list)->used++;
+
+	/*
+		if (used_factors + 2 > size) { // XXX: +2 needed for NULL, or is that an OBOE?
+			// we need more memory to hold this factor
+			uint32_t new_size = size * 5; // XXX: +2 is testing only! XXX: ALWAYS +2 or more, due to the NULL termination below!!! *2 is a bad idea - it's too much. +10?
+//				if (new_size < 10)
+//					new_size = 10;
+			printf("Reallocating; size = %u, needed = %u; new size = %u\n", size, used_factors + 1, new_size);
+			arr = realloc(arr, new_size * sizeof(uint64_t));
+			if (arr == NULL)
+				return NULL;
+			size = new_size;
+		}
+		used_factors++;
+
+		printf("arr = %p to %p, ptr = %p, adding %lu\n", (void *)arr, (void *)(arr+size), (void *)ptr, i);
+		*ptr++ = i; // add this factor to the array
+	*/
+}
+
+// XXX: This needs work - it's hardly obvious that the usage is
+// list_foreach_element(list) { do_something_with(list->arr[i]) } ...
+#define list_foreach_element(list) \
+	for (size_t i = 0; i < (list)->used; i++)
+
+/*
+ * Returns a list of (non-unique) prime factors, i.e. {2, 2, 2, 5} if the input number is 40
+ * orig_n: The number to calculate the factors for.
+ * return value: A preallocated list containing all the prime factors. The caller is resonsible for freeing this.
+ */
+uint64_list *get_prime_factors(const uint64_t orig_n) {
+	uint64_list *list = list_create();
+	if (list == NULL)
+		return NULL;
+	if (orig_n < 2) {
+		return list;
 	}
 
-	uint32_t size = 2; /* start off with a fair guess; XXX: raise this to minimize reallocations */
-	uint64_t *arr = calloc(size, sizeof(uint64_t));
-	if (arr == NULL)
-		return NULL;
-	uint64_t *ptr = arr;
-	uint32_t used_factors = 1; /* count the terminating NUL */
-	// XXX: To sieve or not to sieve? Might be too slow since we need to create a list of primes,
-	// might be faster since we don't need to check every 2nd number here.
-/*
-def factorize(orig_n):
-	factors = [] # done
-	n = orig_n # done
-	i = 2 # done
-	sq_i = i*i # done
-	while sq_i <= n: # done
-		while n % i == 0:
-			n /= i
-			factors.append(i)
-		sq_i += 2 * i + 1
-		i += 1
-	if (n != 1) and (n != orig_n):
-		factors.append(n)
-	
-	return factors
-*/
 	uint64_t n = orig_n;
 	uint64_t i = 2;
 	uint64_t sq_i = i*i;
-    for (; sq_i <= n; i += 1) {  
+    for (; sq_i <= n; i++) {  
 		while (n % i == 0) {
+			list_add(&list, i);
 			n /= i;
-			if (used_factors + 2 > size) { // XXX: +2 needed for NULL, or is that an OBOE?
-				/* we need more memory to hold this factor */
-				uint32_t new_size = size * 5; // XXX: +2 is testing only! XXX: ALWAYS +2 or more, due to the NULL termination below!!! *2 is a bad idea - it's too much. +10?
-//				if (new_size < 10)
-//					new_size = 10;
-				printf("Reallocating; size = %u, needed = %u; new size = %u\n", size, used_factors + 1, new_size);
-				arr = realloc(arr, new_size * sizeof(uint64_t));
-				if (arr == NULL)
-					return NULL;
-				size = new_size;
-			}
-            used_factors++;
-
-			printf("arr = %p to %p, ptr = %p, adding %lu\n", (void *)arr, (void *)(arr+size), (void *)ptr, i);
-			*ptr++ = i; /* add this factor to the array */
         }
-		sq_i += 2 * i + 1; // ?? XXX: memory bounds checking
+		sq_i += 2 * i + 1; // XXX: Why does this work?
     }
 	if (n != 1 && n != orig_n)
-		*ptr++ = n; // XXX: bounds checking
-	printf("NULL terminating: arr = %p to %p, ptr = %p\n", (void *)arr, (void *)(arr+size), (void *)ptr);
-	*ptr++ = 0; /* NULL terminate the array XXX: bounds checking */
+		list_add(&list, n); // XXX: Or this?
 
-	return arr;
+	return list;
 }
 
 int main() {
-	uint64_t *arr = get_prime_factors(NUM);
-	uint64_t i;
-	for (i = 0; arr[i] != 0; i++) 
-		printf("prime factor: %lu\n", arr[i]);
+	uint64_list *list = get_prime_factors(NUM);
+	list_foreach_element(list) {
+		printf("prime factor: %lu\n", list->arr[i]);
+	}
 
-	// The last element of the array is the largest and thus the answer;
-	// -1 is needed due to the above loop setting i to the NULL terminating element.
-	printf("Answer: %lu\n", arr[i-1]);
+	// The last element of the array is the largest and thus the answer; XXX: helper function!
+	printf("Answer: %lu\n", list->arr[list->used-1]);
 
 	printf("Move get_divisors to util.c\n");
 
-	free(arr);
+	list_free(&list);
+
 	return 0;
 }
