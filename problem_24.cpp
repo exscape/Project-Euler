34f5966d5e837b51b3834b3815cf4ba6106aefcb
Thomas <serenity@exscape.org>
2009-11-10 21:16:15 +0100

    Reduce silliness in problem 24 by a lot

--- problem_24.cpp
+++ problem_24.cpp
@@ -1,22 +1,12 @@
 #include <iostream>
 #include <algorithm>
-#include <vector>
 
 int main() {
-	std::vector<short> v;
-	for (short i=0; i<=9; i++)
-		v.push_back(i);
+	char num[] = "0123456789";
+	for (int i=0; i < 999999; i++)
+		std::next_permutation(num, num+10);
 
-	long cnt = 1;
-	while (next_permutation(v.begin(), v.end())) {
-		cnt++;
-		if (cnt == 1000000) {
-			for (short i=0; i<=9; i++)
-				std::cout << v[i]; 
-			std::cout << std::endl;
-			break;
-		}
-	}
+	std::cout << num << std::endl;
 
 	return 0;
 }
